c622b329b9300cb3d65e425db5d9458ffa5ae99e
martin-sutch-softwire <martin.sutch@softwire.com>
2025-07-04 17:14:50 +0100

    Added subpages for individual books (bookdetail) with update and delete book functionality

--- src/main/java/org/softwire/training/bookish/controllers/CatalogueController.java
+++ src/main/java/org/softwire/training/bookish/controllers/CatalogueController.java
@@ -5,9 +5,7 @@ import org.softwire.training.bookish.models.page.CataloguePageModel;
 import org.softwire.training.bookish.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -35,6 +33,12 @@ public class CatalogueController {
         return new ModelAndView("catalogue", "model", cataloguePageModel);
     }
 
+    @GetMapping("/{bookId}")
+    ModelAndView bookDetail(@PathVariable int bookId) {
+        Book book = bookService.getBook(bookId);
+        return new ModelAndView("bookdetail", "book", book);
+    }
+
     @RequestMapping("/add-book")
     RedirectView addBook(@ModelAttribute Book book) {
 
@@ -43,7 +47,14 @@ public class CatalogueController {
         return new RedirectView("/catalogue");
     }
 
-    @RequestMapping("/delete-book")
+    @PostMapping("/{bookId}/edit")
+    RedirectView editBook(@PathVariable int bookId, @ModelAttribute Book book) {
+        book.setBookId(bookId);
+        bookService.updateBook(book);
+        return new RedirectView("/catalogue/" + bookId);
+    }
+
+    @PostMapping("/delete-book")
     RedirectView deleteBook(@RequestParam int bookId) {
 
         bookService.deleteBook(bookId);
--- src/main/java/org/softwire/training/bookish/services/BookService.java
+++ src/main/java/org/softwire/training/bookish/services/BookService.java
@@ -16,6 +16,16 @@ public class BookService extends DatabaseService {
         );
     }
 
+    public Book getBook(int bookId) {
+        return jdbi.withHandle(handle ->
+                handle.createQuery("SELECT * FROM books WHERE book_id = :id")
+                        .bind("id", bookId)
+                        .mapToBean(Book.class)
+                        .findFirst()
+                        .orElse(null)
+                );
+    }
+
     public void addBook(Book book) {
         jdbi.useHandle(handle ->
                 handle.createUpdate("INSERT INTO books (title, author, isbn) VALUES (:title, :author, :isbn)")
@@ -23,6 +33,17 @@ public class BookService extends DatabaseService {
                         .bind("author", book.getAuthor())
                         .bind("isbn", book.getIsbn())
                         .execute()
+        );
+    }
+
+    public void updateBook(Book book) {
+        jdbi.useHandle(handle ->
+                handle.createUpdate("UPDATE books SET title = :title, author = :author, isbn = :isbn WHERE book_id = :id")
+                .bind("title", book.getTitle())
+                .bind("author", book.getAuthor())
+                .bind("isbn", book.getIsbn())
+                .bind("id",book.getBookId())
+                .execute()
         );
     }
 
